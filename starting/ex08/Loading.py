28830fab78878cec1e058a618c82940ab0c21f3d
mudoh <mudoh@student.42.fr>
2024-06-28 17:11:26 +0200

    wtf

--- starting/ex08/Loading.py
+++ starting/ex08/Loading.py
@@ -1,7 +1,18 @@
+import shutil
+import time
+
 def ft_tqdm(lst: range) -> None:
-	
+	terminal_size = int(shutil.get_terminal_size().columns) - 30
 	for i in lst:
-		print(i, "%|\033[47m \033[0m", end='')
+		percent = int(i/(len(lst)-1) * 100)
+		print(percent,"%|", end ='')
+		print(int((percent * terminal_size) /100) * '█', (terminal_size - percent * (len(lst)-i+1)) * "-", end='\r')
+		time.sleep(0.5)
+		yield i
+
 
 if __name__ == "__main__":
-	ft_tqdm(range(70))
+
+	for elem in ft_tqdm(range(15)):
+		time.sleep(0.005)
+	print()
