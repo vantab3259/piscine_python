0657eedf197e9c5ab875fc206112e036f486ee14
mudoh <mudoh@student.42.fr>
2024-06-29 16:19:55 +0200

    presqueok

--- starting/ex08/Loading.py
+++ starting/ex08/Loading.py
@@ -2,17 +2,19 @@ import shutil
 import time
 
 def ft_tqdm(lst: range) -> None:
+	start = time.time()
 	terminal_size = int(shutil.get_terminal_size().columns) - 30
 	for i in lst:
-		percent = int(i/(len(lst)-1) * 100)
-		print(percent,"%|", end ='')
-		print(int((percent * terminal_size) /100) * '█', (terminal_size - percent * (len(lst)-i+1)) * "-", end='\r')
-		time.sleep(0.5)
+		laps = time.time()-start
+		percent = int(((i)/(len(lst))) * 100)
+		first= int((percent * terminal_size) /100)
+		second = terminal_size - first
+		print(f"{percent}%|{first* '█'}{second * ' '}|{i}/{len(lst)} [{int(laps)//60:02}:{int(laps)%60:02}]", end="\r")
 		yield i
+	print(f"100%|{terminal_size * '█'}|{len(lst)}/{len(lst)} [{int(time.time()-start)//60:02}:{int(time.time()-start)%60:02}]")
 
 
 if __name__ == "__main__":
-
-	for elem in ft_tqdm(range(15)):
-		time.sleep(0.005)
-	print()
+	from tqdm import tqdm
+	for elem in tqdm(range(100)):
+		time.sleep(1.0)
