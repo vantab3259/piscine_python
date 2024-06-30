6d9602750ad2a70fbb4ec7b3ebd8c3ac6b22f0e6
mudoh <mudoh@student.42.fr>
2024-06-30 20:20:16 +0200

    fini

--- starting/ex08/Loading.py
+++ starting/ex08/Loading.py
@@ -3,18 +3,19 @@ import time
 
 def ft_tqdm(lst: range) -> None:
 	start = time.time()
-	terminal_size = int(shutil.get_terminal_size().columns) - 30
+	terminal_size = int(shutil.get_terminal_size().columns) - 38
 	for i in lst:
 		laps = time.time()-start
 		percent = int(((i)/(len(lst))) * 100)
 		first= int((percent * terminal_size) /100)
 		second = terminal_size - first
-		print(f"{percent}%|{first* '█'}{second * ' '}|{i}/{len(lst)} [{int(laps)//60:02}:{int(laps)%60:02}]", end="\r")
+		it = f"{(i/laps):.2f} it/s" if i>0 else "],  ?/it"
+		print(f"\r{percent}%|{first* '█'}{second * ' '}|{i}/{len(lst)} [{int(laps)//60:02}:{int(laps)%60:02}],  {it}", end="")
 		yield i
-	print(f"100%|{terminal_size * '█'}|{len(lst)}/{len(lst)} [{int(time.time()-start)//60:02}:{int(time.time()-start)%60:02}]")
+	print(f"\r100%|{terminal_size * '█'}|{len(lst)}/{len(lst)} [{int(time.time()-start)//60:02}:{int(time.time()-start)%60:02}],  {it}")
 
 
 if __name__ == "__main__":
 	from tqdm import tqdm
-	for elem in tqdm(range(100)):
-		time.sleep(1.0)
+	for elem in ft_tqdm(range(5)):
+		time.sleep(0.5)
