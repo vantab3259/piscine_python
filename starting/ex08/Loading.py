32e2def9cdec2aca8c390045cb108c7388cbb8d6
mudoh <mudoh@student.42.fr>
2024-06-30 20:32:04 +0200

    finish

--- starting/ex08/Loading.py
+++ starting/ex08/Loading.py
@@ -10,12 +10,12 @@ def ft_tqdm(lst: range) -> None:
 		first= int((percent * terminal_size) /100)
 		second = terminal_size - first
 		it = f"{(i/laps):.2f} it/s" if i>0 else "],  ?/it"
-		print(f"\r{percent}%|{first* '█'}{second * ' '}|{i}/{len(lst)} [{int(laps)//60:02}:{int(laps)%60:02}],  {it}", end="")
+		print(f"\r  {percent}%|{first* '█'}{second * ' '}|{i}/{len(lst)} [{int(laps)//60:02}:{int(laps)%60:02}],  {it}", end="")
 		yield i
-	print(f"\r100%|{terminal_size * '█'}|{len(lst)}/{len(lst)} [{int(time.time()-start)//60:02}:{int(time.time()-start)%60:02}],  {it}")
+	print(f"\r  100%|{terminal_size * '█'}|{len(lst)}/{len(lst)} [{int(time.time()-start)//60:02}:{int(time.time()-start)%60:02}],  {it}")
 
 
 if __name__ == "__main__":
 	from tqdm import tqdm
-	for elem in ft_tqdm(range(5)):
+	for elem in ft_tqdm(range(100)):
 		time.sleep(0.5)
